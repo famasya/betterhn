f9545df69cb61fcb5b94063a6d99ae40c0ec157a
Abid Famasya <abid.famasya@gmail.com>
2025-09-14 10:13:29 +0700

    bugfix

--- src/components/post-list.tsx
+++ src/components/post-list.tsx
@@ -1,17 +1,26 @@
-import { Link } from "@tanstack/react-router"
-import { ApiResponse } from "../loaders/types"
+import { Link } from "@tanstack/react-router";
+import type { FetchPostApiResponse } from "~/routes/loaders/types";
 
 type Params = {
-  posts: Array<ApiResponse["hits"][number]>
-}
+  posts: Array<FetchPostApiResponse["hits"][number]>;
+};
 export default function PostList({ posts }: Params) {
   // replace all non-alphanumeric characters with a dash
-  const lowerCaseTitle = (title: string) => title.toLowerCase().replace(/[^a-z0-9]/g, '-')
-  return <>
-    {posts.map((post) => (
-      <Link to="/post/$postId" params={{ postId: `${lowerCaseTitle(post.title)}-${post.objectID}` }} key={post.objectID}>
-        <div className="p-3 border-b hover:bg-gray-100 border-gray-200 text-sm">{post.title}</div>
-      </Link>
-    ))}
-  </>
+  const lowerCaseTitle = (title: string) =>
+    title.toLowerCase().replace(/[^a-z0-9]/g, "-");
+  return (
+    <>
+      {posts.map((post) => (
+        <Link
+          to="/post/$postId"
+          params={{ postId: `${lowerCaseTitle(post.title)}-${post.objectID}` }}
+          key={post.objectID}
+        >
+          <div className="p-3 border-b hover:bg-gray-100 border-gray-200 text-sm">
+            {post.title}
+          </div>
+        </Link>
+      ))}
+    </>
+  );
 }
