ee2d874e32511c9a30a3611433b562904a4d1b7d
Abid Famasya <abid.famasya@gmail.com>
2025-09-14 10:10:30 +0700

    add cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,17 @@ import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  server: {
-    port: 3001,
-  },
-  plugins: [
-    tsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tanstackStart({ customViteReactPlugin: true }),
-    viteReact(),
-  ],
+	server: {
+		port: 3001,
+	},
+	plugins: [
+		tsConfigPaths({
+			projects: ['./tsconfig.json'],
+		}),
+		tanstackStart({
+			customViteReactPlugin: true,
+			target: "cloudflare-module"
+		}),
+		viteReact(),
+	],
 })
