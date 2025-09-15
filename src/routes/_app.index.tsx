a16faeab0918caddbafa8d77356b2ae924ee2740
Abid Famasya <abid.famasya@gmail.com>
2025-09-15 08:26:33 +0700

    wip

--- src/routes/_app.index.tsx
+++ src/routes/_app.index.tsx
@@ -2,19 +2,19 @@ import { createFileRoute } from "@tanstack/react-router";
 import { fetchPosts } from "~/lib/fetch-posts";
 
 export const Route = createFileRoute("/_app/")({
-  loader: async () => {
-    const { first10, remainingItems } = await fetchPosts("top");
-    return { first10, remainingItems };
-  },
-  staleTime: 5 * 60 * 1000, // 5 minutes
-  gcTime: 10 * 60 * 1000, // 10 minutes
-  component: RouteComponent,
+	loader: async () => {
+		const { first10, remainingItems } = await fetchPosts("top");
+		return { first10, remainingItems };
+	},
+	staleTime: 5 * 60 * 1000, // 5 minutes
+	gcTime: 10 * 60 * 1000, // 10 minutes
+	component: RouteComponent,
 });
 
 function RouteComponent() {
-  return (
-    <div className="p-2">
-      <h3>hn.fd - Top</h3>
-    </div>
-  )
+	return (
+		<div className="p-2">
+			<h3>hn.fd - Top</h3>
+		</div>
+	);
 }
