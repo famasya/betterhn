a777db665c9e47ae18612364fbfb6a601a6a6cec
Abid Famasya <abid.famasya@gmail.com>
2026-03-13 00:54:49 +0700

    chore: remove build artifacts from dist directory

--- dist/server/index.js
+++
@@ -1,1 +0,0 @@
-import{F as e,E as r}from"./assets/worker-entry-BnlfbnEb.js";import"node:events";import"node:stream";import"node:async_hooks";import"node:stream/web";export{e as createServerEntry,r as default};
